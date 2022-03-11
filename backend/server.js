1b27239897b6e1d8c2ed6a82089c055d373a57d8
jackoyau <jackoyau555@gmail.com>
2022-03-12 03:33:42 +0800

    setup socketio and get websocket data from binance api

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,55 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import http from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
+import Binance from 'node-binance-api'
 import stockRoutes from './routes/stockRoutes.js'
 
 dotenv.config()
+const PORT = process.env.PORT || 5000
+const binance = new Binance().options({
+    APIKEY: process.env.KEY,
+    APISECRET: process.env.SECRET,
+})
 
 const app = express()
+app.use(cors())
 app.use(express.json())
-
 app.use('/api/stocks', stockRoutes)
 
+const server = http.createServer()
+const io = new Server(server,{
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+      }
+})   
+io.on('connection', socket => {
+      console.log('success connect!') 
+      socket.on('getMessage', message => {
+        console.log('server recieved client msg')
+        socket.emit('getMessage', message)
+        binance.websockets.chart("BNBBTC", "1m", (symbol, interval, chart) => {
+            let tick = binance.last(chart)
+            const last = chart[tick].close
+            socket.emit('getMessage',chart)
+            // Optionally convert 'chart' object to array:
+            // let ohlc = binance.ohlc(chart);
+            // console.info(symbol, ohlc);
+            socket.emit('getMessage', symbol+" last price: "+last)
+        })
+        binance.websockets.prevDay('BNBBTC', (error, response) => {
+            socket.emit('getMessage', response)
+          });
+    })
+})
+server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode and socket.io on port ${PORT}`))
 
 
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,23 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { io } from 'socket.io-client'
+
 
 const HomeScreen = () => {
   const[stocks, setStocks] = useState()
-  
-  useEffect(()=>{
-    const intervalId = setInterval(() => {
-      const getData = async ()=>{
-        const { data } = await axios.get('/api/stocks')
-        console.log (data)
-        setStocks(data)
+  const[ws, setWs] = useState(null)
+  const connectWebSocket = () => {
+    setWs(io('http://localhost:5000', {
+      withCredentials: true,
+      extraHeaders: {
+        "my-custom-header": "abcd"
       }
-      getData()
-    }, 1000)
-    return () => clearInterval(intervalId);
-  },[])
+    }))
+}
+  
+const initWebSocket = () => {
+  ws.on('getMessage', message => {
+      console.log(message)
+  })
+}
+const sendMessage = () => {
+  ws.emit('getMessage', {name: 'jacko'})
+}
+
+  useEffect(()=>{
+    // const intervalId = setInterval(() => {
+    //   const getData = async ()=>{
+    //     const { data } = await axios.get('/api/stocks')
+    //     setStocks(data)
+    //   }
+    //   getData()
+    // }, 1000)
+    // return () => clearInterval(intervalId);
+    if(ws){
+      
+      console.log('client success connect!')
+      initWebSocket()
+    }
+  },[ws])
   
   return (
-    <h1>{stocks}</h1>
+    <>
+    {/* <h1>{stocks}</h1> */}
+    <input type='button' value='connect' onClick={connectWebSocket} />
+    <input type='button' value='sendMessage' onClick={sendMessage} />
+    </>
   )
 }
 
